6256aacc76cd4df3c68a739613e2a576526e30fe
ajay <ajaykantkanojiya@gmail.com>
2026-01-03 21:11:29 +0530

    work on confrigation local adn servr for new all work 3-1-2026

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base = import.meta.env.VITE_API_BASE || "/v1";
+const base = import.meta.env.VITE_API_BASE || "/api/v1";
 
 const api = axios.create({
   baseURL: base,
@@ -14,17 +14,3 @@ const token = localStorage.getItem("token");
 if (token) api.defaults.headers.common["Authorization"] = "Bearer " + token;
 
 export default api;
-
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: '/api/v1',  // relative path    proxy: {
-//       '/api': {
-//         target: 'http://localhost:8000',
-//         changeOrigin: true,
-//       },);
-// console.log(baseURL);
-// // attach token if exists
-// const token = localStorage.getItem("token");
-
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/v1': {
+      '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
       },
